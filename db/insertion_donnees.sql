a4e26fefaba6e10e14b1e76f7ac60d057dc4cc22
mrq0340a <34028266+mrq0340a@users.noreply.github.com>
2017-11-30 21:27:14 +0100

    Update insertion_donnees.sql
    insertion des données fixed

--- db/insertion_donnees.sql
+++ db/insertion_donnees.sql
@@ -1,27 +1,38 @@
 -- table `groupe`
 INSERT INTO `groupe` (`id_groupe`, `nom_groupe`) VALUES
-(1, 'univtlse3');
+(1, 'TDA1'),
+(2, 'TDA2'),
+(3, 'Univ-Tlse3');
 
 -- table `user`
-INSERT INTO `user` (`id_user`, `nom_user`, `prenom_user`) VALUES
-(1, 'pipo', 'pomme', 'pmp'),
-(2, 'cuma', 'ty', 'mrq');
-(3, 'fougere', 'race', 'fgv');
-
--- table `ticket`
-INSERT INTO `ticket` (`id_ticket`, `titre_ticket`, `creation_ticket`) VALUES
-(1, 'Un ticket simple', '2017-11-08 13:09:19');
-
--- table `message`
-INSERT INTO `message` VALUES
-(1, 1, 1, 'wsh c\'est pas normal le contrôle de graphe était trop dur, j\'ai dû tout copier sur philibert', '2017-11-08 10:00:00', '0');
+INSERT INTO `user` (`id_user`, `nickname_user`, `nom_user`, `prenom_user`) VALUES
+(1, 'pipo1118m', 'Paumerai', 'Peter'),
+(2, 'vifo0077i', 'Fougere', 'Vins'),
+(3, 'cuma0340a', 'Marty', 'Hubert');
 
 -- table `appartenance`
 INSERT INTO `appartenance` (`id_user`, `id_groupe`, `inscription`) VALUES
-(1, 1, '2017-11-08 00:00:00'),
-(2, 1, '2017-11-08 00:00:00');
+(1, 2, '2017-11-01 00:00:00'),
+(1, 3, '2017-11-01 00:00:00'),
+(2, 2, '2017-11-01 00:00:00'),
+(2, 3, '2017-11-03 00:00:00'),
+(3, 1, '2017-11-01 00:00:00'),
+(3, 3, '2017-11-02 00:00:00');
+
+-- table `ticket`
+INSERT INTO `ticket` (`id_ticket`, `titre_ticket`, `creation_ticket`, `id_groupe`) VALUES
+(1, 'Chauffage', '2017-11-08 00:00:00', 3);
+
+-- table `message`
+INSERT INTO `message` (`id_message`, `id_ticket`, `id_user`, `contenu`, `date_message`) VALUES
+(1, 1, 2, 'Le chauffe marche plus, il fait froid. Reparez le !', '2017-11-14 00:00:00'),
+(2, 1, 1, 'Ouais c\'est pas faux !', '2017-11-15 00:00:00');
 
 -- table `status`
-INSERT INTO `status` (`id_message`, `id_user`, `status`) VALUES
-(1, 1, '0'),
-(1, 2, '2');
+INSERT INTO `statut` (`id_message`, `id_user`, `statut`) VALUES
+(1, 1, '2'),
+(1, 2, '2'),
+(1, 3, '1'),
+(2, 1, '2'),
+(2, 2, '0'),
+(2, 3, '0');
