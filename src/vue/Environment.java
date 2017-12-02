a325f3af4b973e94d074a531d69d0e4b59330fd8
Vincent Fougeras <fougeras.vincent@gmail.com>
2017-12-02 11:35:40 +0100

    Création des vues de l'écran de connexion et de l'écran principal

---
+++ src/vue/Environment.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vue;
+
+/**
+ *
+ * @author Vincent Fougeras
+ */
+public class Environment {
+    public static final String APP_NAME = "Chat S5";
+}
