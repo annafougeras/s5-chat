cbb5b114932cb1d0bac4a12d479cc9e18c89cacb
mrq0340a <34028266+mrq0340a@users.noreply.github.com>
2018-01-15 19:00:24 +0100

    Update insertion_donnees.sql

--- db/insertion_donnees.sql
+++ db/insertion_donnees.sql
@@ -1,8 +1,16 @@
 -- table `groupe`
 INSERT INTO `groupe` (`id_groupe`, `nom_groupe`) VALUES
-(1, 'TDA1'),
-(2, 'TDA2'),
-(3, 'Univ-Tlse3');
+(1, 'Groupe 1.1'),
+(2, 'Groupe 1.2'),
+(3, 'Groupe 2.1'),
+(4, 'Groupe 2.2'),
+(5, 'Groupe 3.1'),
+(6, 'Groupe 3.2'),
+(7, 'Groupe 4.1'),
+(8, 'Groupe 4.2'),
+(9, 'Secretariat'),
+(10, 'Corpo Sciences'),
+(11, 'Univ-Tlse3');
 
 -- table `user` mdp : root1 (grain de sel : "qt" && grain de poivre : "pi")
 INSERT INTO `user` (`id_user`, `nickname_user`, `password_user`, `nom_user`, `prenom_user`) VALUES
@@ -14,26 +22,27 @@ INSERT INTO `user` (`id_user`, `nickname_user`, `password_user`, `nom_user`, `pr
 -- table `appartenance`
 INSERT INTO `appartenance` (`id_user`, `id_groupe`, `inscription`) VALUES
 (1, 2, '2017-11-01 00:00:00'),
-(1, 3, '2017-11-01 00:00:00'),
+(1, 10, '2017-11-01 00:00:00'),
+(1, 11, '2017-11-01 00:00:00'),
 (2, 2, '2017-11-01 00:00:00'),
-(2, 3, '2017-11-03 00:00:00'),
-(3, 1, '2017-11-01 00:00:00'),
-(3, 3, '2017-11-02 00:00:00');
+(2, 11, '2017-11-03 00:00:00'),
+(3, 2, '2017-11-01 00:00:00'),
+(3, 11, '2017-11-02 00:00:00');
 
 -- table `ticket`
 INSERT INTO `ticket` (`id_ticket`, `titre_ticket`, `creation_ticket`, `id_groupe`) VALUES
-(1, 'Chauffage', '2017-11-08 00:00:00', 3);
+(1, 'Chauffage', '2017-11-08 00:00:00', 9);
 
 -- table `message`
 INSERT INTO `message` (`id_message`, `id_ticket`, `id_user`, `contenu`, `date_message`) VALUES
-(1, 1, 2, 'Le chauffe marche plus, il fait froid. Reparez le !', '2017-11-14 00:00:00'),
-(2, 1, 1, 'Ouais c\'est pas faux !', '2017-11-15 00:00:00');
+(1, 1, 1, 'Le chauffe marche plus, il fait froid. Reparez le !', '2017-11-14 00:00:00'),
+(2, 1, 3, 'Ouais c\'est pas faux !', '2017-11-15 00:00:00');
 
 -- table `status`
 INSERT INTO `statut` (`id_message`, `id_user`, `statut`) VALUES
 (1, 1, '2'),
-(1, 2, '2'),
-(1, 3, '1'),
-(2, 1, '2'),
-(2, 2, '0'),
-(2, 3, '0');
+(1, 2, '0'),
+(1, 3, '2'),
+(2, 1, '1'),
+(2, 2, '1'),
+(2, 3, '2');
